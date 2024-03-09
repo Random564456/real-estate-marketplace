11aa6a6488a3f2dc0fd2f00757dd4435e30f6b3b
Alex Vrsecky <vrsecky102@gmail.com>
2024-03-09 19:16:58 +1100

    fixed signup bug

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -70,7 +70,7 @@ const SignUp = () => {
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to="/sing-in">
+        <Link to="/sign-in">
           <span className="text-blue-700">Sing In</span>
         </Link>
       </div>
